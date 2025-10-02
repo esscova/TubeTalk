aa6395db7c400c1b5044746878acbc7d4ee1c08f
esscova <wsantos08@hotmail.com>
2025-10-02 20:31:21 -0300

    refact: configurações de página transferidas para classe ui

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ import streamlit as st
 from UI import UI
 
 if __name__ == "__main__":
-    st.set_page_config(page_title="TubeTalk", page_icon=":robot_face:", layout="centered")
     ui = UI()
     ui.run()
