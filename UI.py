6667b99c7d12f8b6768ab4142dd62b97f4f6b8da
esscova <wsantos08@hotmail.com>
2025-10-03 18:36:17 -0300

    feat: remoção e  adição de metadados do video

--- UI.py
+++ UI.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from services.youtube_video import get_video_info
 
 class UI:
     def __init__(self):
@@ -22,10 +23,11 @@ class UI:
                     background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
                 }
                 .video-section{
+                    text-align: center;
                     background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
                     padding: 0.5rem;
                     border-radius: 10px;
-                    margin: 1rem 0;
+                    margin: 2rem 0;
                 }
             </style>
         """, unsafe_allow_html=True)
@@ -43,6 +45,9 @@ class UI:
                             <h2>🤖 Your personal YouTube assistant</h2>
                             <p>Analyze YouTube videos with ease!</p>
                         </div>
+                        <p style='text-align: center; margin-top: 3rem;'>Developed by 
+                            <a href='https://www.linkedin.com/in/wellington-moreira-santos' target='_blank'>Wellington M Santos</a>
+                        </p>
                     """, unsafe_allow_html=True)
 
         if not st.session_state.submitted:
@@ -62,7 +67,7 @@ class UI:
                     st.rerun()
                 else:
                     st.error("Please enter a valid YouTube video URL.")
-            st.markdown("<p style='text-align: center; margin-top: 3rem;'>Developed by <a href='https://www.linkedin.com/in/wellington-moreira-santos' target='_blank'>Wellington M Santos</a></p>", unsafe_allow_html=True)
+            #st.markdown("<p style='text-align: center; margin-top: 3rem;'>Developed by <a href='https://www.linkedin.com/in/wellington-moreira-santos' target='_blank'>Wellington M Santos</a></p>", unsafe_allow_html=True)
         else:
             url = st.session_state.video_url
             try:
@@ -73,14 +78,21 @@ class UI:
                 else:
                     raise ValueError("Invalid URL format")
                 
+                video_info = get_video_info(url)
                 st.markdown(f"""
-                                <div style='text-align: center;'>
+                                <div>
                                     <h2 style='padding: 1rem;'>Here is the video you submitted:</h2>
                                     <iframe width='100%' height='315' src='https://www.youtube.com/embed/{video_id}' frameborder='0' style='margin-bottom: 1rem;'></iframe>
                                     <h2 class="video-section">Information about the video</h2>
-                                    <p style='font-size: 1.2rem;'>Video ID: <strong>{video_id}</strong></p>
-                                    <p style='font-size: 1.2rem;'>Title: <strong>Placeholder Title</strong></p>
-                                    <p style='font-size: 1.2rem;'>Description: <strong>Placeholder Description</strong></p>
+                                    <div style='margin-top: 1rem;'>
+                                        <p><strong>URL:</strong> {url}</p>
+                                        <p><strong>Video ID:</strong> {video_id}</p>
+                                        <p><strong>Title:</strong> {video_info['title']}</p>
+                                        <p><strong>Author:</strong> {video_info['author']}</p>
+                                        <p><strong>Published on:</strong> {video_info['publish_date']}</p>
+                                        <p><strong>Views:</strong> {video_info['views']}</p>
+                                        <p><strong>Palavras-chave:</strong> {', '.join(video_info['keywords']) if video_info['keywords'] else 'N/A'}</p>
+                                    </div>
                                     <hr style='margin: 2rem 0;' />                                    
                                     <h2 class="video-section">Summary</h2>
                                     <hr style='margin: 2rem 0;' />                                    
