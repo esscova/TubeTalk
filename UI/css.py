a38e8ff9548e1f2c585a1c79d1f795fa637f4d45
esscova <wsantos08@hotmail.com>
2025-10-03 19:09:22 -0300

    refact: modularizando trechos html e css para interface

---
+++ UI/css.py
@@ -0,0 +1,18 @@
+STYLE = """
+            <style>
+                .header-area{
+                    text-align: center;
+                    margin-bottom: 2rem;
+                    padding: 1rem;
+                    border-radius: 10px;
+                    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
+                }
+                .video-section{
+                    text-align: center;
+                    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
+                    padding: 0.5rem;
+                    border-radius: 10px;
+                    margin: 2rem 0;
+                }
+            </style>
+        """
--- UI/index.py
+++ UI/index.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from UI.css import STYLE
+from UI.html_snippet import HEADER, render_video_info, FOOTER
 from services.youtube_video import get_video_info
 
 class UI:
@@ -13,24 +15,7 @@ class UI:
         st.set_page_config(page_title="TubeTalk", page_icon="💡", layout="centered")
 
         # Estilos personalizados
-        st.markdown("""
-            <style>
-                .header-area{
-                    text-align: center;
-                    margin-bottom: 2rem;
-                    padding: 1rem;
-                    border-radius: 10px;
-                    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
-                }
-                .video-section{
-                    text-align: center;
-                    background: linear-gradient(135deg, #f6d365 0%, #fda085 100%);
-                    padding: 0.5rem;
-                    border-radius: 10px;
-                    margin: 2rem 0;
-                }
-            </style>
-        """, unsafe_allow_html=True)
+        st.markdown(STYLE, unsafe_allow_html=True)
 
     def reset(self):
         """Reseta o estado da sessão"""
@@ -39,16 +24,7 @@ class UI:
     
     def run(self):
         """Executa a interface principal"""
-        st.markdown("""
-                        <div class="header-area">
-                            <h1>TubeTalk</h1>
-                            <h2>🤖 Your personal YouTube assistant</h2>
-                            <p>Analyze YouTube videos with ease!</p>
-                        </div>
-                        <p style='text-align: center; margin-top: 3rem;'>Developed by 
-                            <a href='https://www.linkedin.com/in/wellington-moreira-santos' target='_blank'>Wellington M Santos</a>
-                        </p>
-                    """, unsafe_allow_html=True)
+        st.markdown(HEADER, unsafe_allow_html=True)
 
         if not st.session_state.submitted:
             st.markdown("<p style='font-size: 1.5rem; font-weight: bold; text-align: center; '>Enter a YouTube video URL to get started:</p>", unsafe_allow_html=True)
@@ -67,7 +43,7 @@ class UI:
                     st.rerun()
                 else:
                     st.error("Please enter a valid YouTube video URL.")
-            #st.markdown("<p style='text-align: center; margin-top: 3rem;'>Developed by <a href='https://www.linkedin.com/in/wellington-moreira-santos' target='_blank'>Wellington M Santos</a></p>", unsafe_allow_html=True)
+        
         else:
             url = st.session_state.video_url
             try:
@@ -79,28 +55,7 @@ class UI:
                     raise ValueError("Invalid URL format")
                 
                 video_info = get_video_info(url)
-                st.markdown(f"""
-                                <div>
-                                    <h2 style='padding: 1rem;'>Here is the video you submitted:</h2>
-                                    <iframe width='100%' height='315' src='https://www.youtube.com/embed/{video_id}' frameborder='0' style='margin-bottom: 1rem;'></iframe>
-                                    <h2 class="video-section">Information about the video</h2>
-                                    <div style='margin-top: 1rem;'>
-                                        <p><strong>URL:</strong> {url}</p>
-                                        <p><strong>Video ID:</strong> {video_id}</p>
-                                        <p><strong>Title:</strong> {video_info['title']}</p>
-                                        <p><strong>Author:</strong> {video_info['author']}</p>
-                                        <p><strong>Published on:</strong> {video_info['publish_date']}</p>
-                                        <p><strong>Views:</strong> {video_info['views']}</p>
-                                        <p><strong>Palavras-chave:</strong> {', '.join(video_info['keywords']) if video_info['keywords'] else 'N/A'}</p>
-                                    </div>
-                                    <hr style='margin: 2rem 0;' />                                    
-                                    <h2 class="video-section">Summary</h2>
-                                    <hr style='margin: 2rem 0;' />                                    
-                                    <h2 class="video-section">Themes</h2>
-                                    <hr style='margin: 2rem 0;' />                                    
-                                </div>
-                            """, unsafe_allow_html=True)
-
+                st.markdown(render_video_info(url, video_id, video_info), unsafe_allow_html=True)
 
             except Exception as e:
                 st.warning(f"Could not load video thumbnail: {e}")
@@ -110,3 +65,5 @@ class UI:
             if st.button("Analyze Another Video"):
                 self.reset()
                 st.rerun()
+        
+        #st.markdown(FOOTER, unsafe_allow_html=True)
